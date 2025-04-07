154ad69a22a8a1eafaccf1744fcb6b1f8465c3c5
Peter Robinson <drinkcoffee@eml.cc>
2025-04-07 16:04:33 +1000

    Use IImmutableERC721V3 to get version

--- script/ERC721Migration.s.sol
+++ script/ERC721Migration.s.sol
@@ -5,6 +5,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {SampleERC721} from "../src/SampleERC721.sol";
 import {SampleERC721Bootstrap} from "../src/SampleERC721Bootstrap.sol";
 import {IImmutableERC721, IImmutableERC721Errors} from "@imtbl/contracts/token/erc721/interfaces/IImmutableERC721.sol";
+import {IImmutableERC721V3} from "@imtbl/contracts/token/erc721/interfaces/IImmutableERC721V3.sol";
 import {ImmutableERC721MintByIDBootstrapV3} from
     "@imtbl/contracts/token/erc721/preset/ImmutableERC721MintByIDBootstrapV3.sol";
 import {ImmutableERC721MintByIDUpgradeableV3} from
@@ -54,66 +55,81 @@ contract ERC721MigrationScript is Script {
         ERC1967Proxy proxy = new ERC1967Proxy(address(bootstrapImpl), initData);
         console.log("Deployed proxy to: %x", address(proxy));
         vm.stopBroadcast();
+
+        console.log("Deploy bootstrap and proxy complete");
     }
 
 
-    function mintAndSetRoyalties(address proxy, address user1, address user2) public {
+    // NOTE: NFTs must be minted before this call.
+    function setRoyalties(address _proxy, uint256 _tokenId) public {
         address owner = OWNER;
         address royaltyReceiver = owner;
 
-        IImmutableERC721 erc721 = IImmutableERC721(proxy);
+        IImmutableERC721 erc721Bootstrap = IImmutableERC721(_proxy);
 
-        // Mint some NFTs
-        IImmutableERC721.IDMint[] memory mintRequests = new IImmutableERC721.IDMint[](2);
-        uint256[] memory tokenIds1 = new uint256[](4);
-        tokenIds1[0] = 100;
-        tokenIds1[1] = 101;
-        tokenIds1[2] = 102;
-        tokenIds1[3] = 1000;
-        uint256[] memory tokenIds2 = new uint256[](3);
-        tokenIds2[0] = 103;
-        tokenIds2[1] = 1001;
-        tokenIds2[2] = 1002;
-        mintRequests[0].to = user1;
-        mintRequests[0].tokenIds = tokenIds1;
-        mintRequests[1].to = user2;
-        mintRequests[1].tokenIds = tokenIds2;
-        vm.startBroadcast();
-        erc721.mintBatch(mintRequests);
-        
-        // Set fee for NFTs 1000 to 1002 to 5%
-        uint96 feeNumerator = 200; // 2%
         uint96 otherFeeNumerator = 500;
-        uint256[] memory nfts = new uint256[](3);
-        for (uint256 i = 0; i < nfts.length; i++) {
-            nfts[i] = 1000 + i;
-        }
+        uint256[] memory nfts = new uint256[](1);
+        nfts[0] = _tokenId;
+
         // Must be called by minter
-        erc721.setNFTRoyaltyReceiverBatch(nfts, royaltyReceiver, otherFeeNumerator);
+        vm.startBroadcast();
+        erc721Bootstrap.setNFTRoyaltyReceiverBatch(nfts, royaltyReceiver, otherFeeNumerator);
         vm.stopBroadcast();
 
         // Do some checks
-        require(erc721.balanceOf(user1) == 4, "Balance user1 after initial mint");
-        require(erc721.balanceOf(user2) == 3, "Balance user2 after initial mint");
-        require(erc721.totalSupply() == 7, "Total supply after initial mint");
-        require(erc721.ownerOf(100) == user1, "Owner of 100 after initial mint");
-        require(erc721.ownerOf(101) == user1, "Owner of 101 after initial mint");
-        require(erc721.ownerOf(102) == user1, "Owner of 102 after initial mint");
-        require(erc721.ownerOf(1000) == user1, "Owner of 1000 after initial mint");
-        require(erc721.ownerOf(103) == user2, "Owner of 103 after initial mint");
-        require(erc721.ownerOf(1001) == user2, "Owner of 1001 after initial mint");
-        require(erc721.ownerOf(1002) == user2, "Owner of 1002 after initial mint");
+        (address receiver, uint256 royaltyAmount) = erc721Bootstrap.royaltyInfo(_tokenId, 10000);
+        require(receiver == royaltyReceiver, "Wrong receiver2");
+        require(royaltyAmount == otherFeeNumerator, "Wrong fee2");
 
-        for (uint256 i = 100; i < 104; i++) {
-            (address receiver, uint256 royaltyAmount) = erc721.royaltyInfo(i, 10000);
-            require(receiver == royaltyReceiver, "Wrong receiver1");
-            require(royaltyAmount == feeNumerator, "Wrong fee1");
-        }
-        for (uint256 i = 1000; i < 103; i++) {
-            (address receiver, uint256 royaltyAmount) = erc721.royaltyInfo(i, 10000);
-            require(receiver == royaltyReceiver, "Wrong receiver2");
-            require(royaltyAmount == otherFeeNumerator, "Wrong fee2");
-        }
+        console.log("setNFTRoyaltyReceiverBatch complete");
     }
 
+    // NOTE: NFTs must be minted before this call.
+    function bootstrapChangeOwnership(address _proxy, uint256 _tokenId, address _newOwner) public {
+        SampleERC721Bootstrap erc721Bootstrap = SampleERC721Bootstrap(_proxy);
+
+        address oldTokenOwner = erc721Bootstrap.ownerOf(_tokenId);
+
+        // Change ownership of some NFTs
+        ImmutableERC721MintByIDBootstrapV3.BootstrapTransferRequest[] memory requests =
+            new ImmutableERC721MintByIDBootstrapV3.BootstrapTransferRequest[](1);
+        ImmutableERC721MintByIDBootstrapV3.BootstrapTransferRequest memory request1 =
+            ImmutableERC721MintByIDBootstrapV3.BootstrapTransferRequest({from: oldTokenOwner, to: _newOwner, tokenId: _tokenId});
+        requests[0] = request1;
+
+        vm.startBroadcast();
+        erc721Bootstrap.bootstrapPhaseChangeOwnership(requests);
+        vm.stopBroadcast();
+
+        console.log("bootstrapPhaseChangeOwnership complete");
+    }
+
+
+
+    function deployERC721() public {
+        vm.startBroadcast();
+        SampleERC721 erc721 = new SampleERC721();
+        vm.stopBroadcast();
+
+        console.log("Deployed erc721 implementation to: %x", address(erc721));
+    }
+    
+
+    function upgrade(address _proxy, address _erc721Impl) public {
+        vm.startBroadcast();
+        SampleERC721Bootstrap erc721Bootstrap = SampleERC721Bootstrap(_proxy);
+
+        // Execute upgrade
+        // A function must be called, so just call the balanceOf view function.
+        bytes memory initData = abi.encodeWithSelector(ERC721Upgradeable.balanceOf.selector, address(1));
+
+        vm.startBroadcast();
+        erc721Bootstrap.upgradeToAndCall(address(_erc721Impl), initData);
+        vm.stopBroadcast();
+
+        IImmutableERC721V3 erc721 = IImmutableERC721V3(_proxy);
+        require(erc721.version() == 1, "version");
+
+        console.log("Upgrade complete");
+    }
 }
